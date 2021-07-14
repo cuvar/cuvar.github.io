362f948c905b7f669435b66c18d9e1bf120c9ecb
Luca Müller <luca-mueller@gmx.net>
2021-07-14 12:52:44 +0200

    init: added bootstrap

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../res/logo.svg";
+import "../style/App.scss";
 
 function App() {
   return (
