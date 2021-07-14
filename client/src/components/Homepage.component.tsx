eb4057794754a0b7ef55555555a18af6205b4d52
Luca Müller <luca-mueller@gmx.net>
2021-07-14 15:25:53 +0200

    feat: added color palette

--- client/src/components/Homepage.component.tsx
+++ client/src/components/Homepage.component.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+// import "./style/Homepage.scss";
 
 class Homepage extends Component {
     render() {
         return (
-        <div>
-            Hello world
+        <div className="text-center display-4">
+            <span className="one">This website </span>
+            <span className="two">is currently </span>
+            <span className="three">under construction</span>
         </div>
         );
     }
